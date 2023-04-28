f1355888d55f3387b1a1bec0198ce3d394561967
nimtiaz-ucsc <nimtiaz@ucsc.edu>
2023-04-27 23:20:39 -0700

    progress on monster generator, name and homeland

---
+++ assignment3/monsters.js
@@ -0,0 +1,25 @@
+{
+    "name1": ["Ghi", "God", "Go", "Ro", "Moth", "Mecha", "Destro", "Goro", "Tyranno", "King #name1#", "Mega", "Igua", "Veloci", "Carno", "Star", "Robo", "Abominable #name1#", "La", "Ence", "Destructo", "Beta", "Vil", "Jaka", "Ry", "Titani", "Titano", "Demon #name1#", "Thunder", "Blazing #name1#", "Alpha", "Mino", "Cen", "Manti", "Char", "Cer", "Cy", "Scy"],
+    
+    "name2": ["do", "ji", "ro", "lo", "ky", "ko", "ber", "no"],
+    
+    "name3": ["rah", "zilla", "ra", "dan", "izer", "yah", "saurus", "lon", "don", "nodon", "raptor", "taurus", "oid", "tron", "don", "ladus", "rax", "gax", "rak", "kon", "saur", "#name3# King", "on", "ron", "claw", "con", "wing", "ous", "rous", "taur", "os", "phon", "us", "clops", "lla", "mon"],
+    
+    "name": ["#name1##name2#", "#name1##name3#", "#name1##name2##name3#", "#name1##name2##name2##name3#"],
+    
+    "lowHome": ["the depths below", "dark caverns unkwown", "hell itself", "deep underground", "depths unknown", "the planet's core", "an active volcano", "murky swamplands", "the shadows", "the deep sea", "the ocean's depths", "the sea floor", "beneath the surface"],
+    
+    "highHome": ["the stars above", "the skies above", "above the clouds", "a galaxy far far away", "the farthest reaches of outer space", "the upper atmosphere"],
+    
+    "home": ["#lowHome#", "#highHome#", "distant lands", "lands unknown", "freezing tundras", "sweltering deserts", "dense jungles", "vast grasslands"],
+    
+    "lowVerb": ["Arising", "Rising up", "Emerging"],
+    
+    "highVerb": ["Falling", "Dropping in", "Descending", "Plummeting", "Tumbling in"],
+    
+    "homeVerb": ["Hailing", "Coming", "Originating"],
+    
+    "homeSentence": ["#lowVerb# from #lowHome#", "#highVerb# from #highHome#", "#homeVerb# from #home#"],
+    
+    "monster":["#homeSentence#, #name# approaches..."]
+    }
