65e4cc1f17bfc083b579391eb229e14a694bd37a
nimtiaz-ucsc <nimtiaz@ucsc.edu>
2023-04-28 20:00:37 -0700

    finished monster grammar

--- assignment3/monsters.js
+++ assignment3/monsters.js
@@ -1,25 +1,24 @@
 {
-    "name1": ["Ghi", "God", "Go", "Ro", "Moth", "Mecha", "Destro", "Goro", "Tyranno", "King #name1#", "Mega", "Igua", "Veloci", "Carno", "Star", "Robo", "Abominable #name1#", "La", "Ence", "Destructo", "Beta", "Vil", "Jaka", "Ry", "Titani", "Titano", "Demon #name1#", "Thunder", "Blazing #name1#", "Alpha", "Mino", "Cen", "Manti", "Char", "Cer", "Cy", "Scy"],
-    
-    "name2": ["do", "ji", "ro", "lo", "ky", "ko", "ber", "no"],
-    
-    "name3": ["rah", "zilla", "ra", "dan", "izer", "yah", "saurus", "lon", "don", "nodon", "raptor", "taurus", "oid", "tron", "don", "ladus", "rax", "gax", "rak", "kon", "saur", "#name3# King", "on", "ron", "claw", "con", "wing", "ous", "rous", "taur", "os", "phon", "us", "clops", "lla", "mon"],
-    
-    "name": ["#name1##name2#", "#name1##name3#", "#name1##name2##name3#", "#name1##name2##name2##name3#"],
-    
-    "lowHome": ["the depths below", "dark caverns unkwown", "hell itself", "deep underground", "depths unknown", "the planet's core", "an active volcano", "murky swamplands", "the shadows", "the deep sea", "the ocean's depths", "the sea floor", "beneath the surface"],
-    
-    "highHome": ["the stars above", "the skies above", "above the clouds", "a galaxy far far away", "the farthest reaches of outer space", "the upper atmosphere"],
-    
-    "home": ["#lowHome#", "#highHome#", "distant lands", "lands unknown", "freezing tundras", "sweltering deserts", "dense jungles", "vast grasslands"],
-    
-    "lowVerb": ["Arising", "Rising up", "Emerging"],
-    
-    "highVerb": ["Falling", "Dropping in", "Descending", "Plummeting", "Tumbling in"],
-    
-    "homeVerb": ["Hailing", "Coming", "Originating"],
-    
-    "homeSentence": ["#lowVerb# from #lowHome#", "#highVerb# from #highHome#", "#homeVerb# from #home#"],
-    
-    "monster":["#homeSentence#, #name# approaches..."]
-    }
+"name1":["Ghi", "God", "Go", "Ro", "Moth", "Mecha", "Destro", "Goro", "Tyranno", "King #name1#", "Mega", "Igua", "Veloci", "Carno", "Star", "Robo", "Abominable #name1#", "La", "Ence", "Destructo", "Beta", "Vil", "Jaka", "Ry", "Titani", "Titano", "Demon #name1#", "Thunder", "Blazing #name1#", "Alpha", "Mino", "Cen", "Manti", "Char", "Cer", "Cy", "Scy", "Fro", "Avi", "Aqua", "Ta", "Qi", "Super #name1#", "Pyro", "Furious #name1#", "Oro"],
+"name2":["do", "ji", "ro", "lo", "ky", "ko", "ber", "no", "ga", "go", "gu", "shi", "ra", "rah", "ya", "yah", "gue", "cu", "yo", "zo", "mo", "ia", "la", "mu", "mi", "chi"],
+"name3":["zilla", "dan", "izer", "aizer", "zer", "saurus", "lon", "don", "nodon", "raptor", "taurus", "oid", "tron", "don", "ladus", "rax", "gax", "rak", "kon", "saur", "#name3# King", "on", "ron", "claw", "con", "wing", "ous", "rous", "taur", "os", "phon", "us", "clops", "lla", "mon", "tan", "ras", "zora", "gon", " Rex", " Prime", " X", "los", "dius", "nius", "mera", "head", "vum", "nator", "roc", "zoid", "maru"],
+"name":["#name1##name2#", "#name1##name2##name2#", "#name1##name3#", "#name1##name2##name3#", "#name1##name2##name2##name3#"],
+"lowHome":["the depths below", "dark caverns unkwown", "hell itself", "deep underground", "depths unknown", "the planet's core", "an active volcano", "murky swamplands", "the shadows", "the deep sea", "the ocean's depths", "the sea floor", "beneath the surface"],
+"highHome":["the stars above", "the skies above", "above the clouds", "a galaxy far far away", "the farthest reaches of outer space", "the upper atmosphere"],
+"home":["#lowHome#", "#highHome#", "distant lands", "lands unknown", "freezing tundras", "sweltering deserts", "dense jungles", "vast grasslands"],
+"lowVerb":["Arising", "Rising up", "Emerging"],
+"highVerb":["Falling", "Dropping in", "Descending", "Plummeting", "Tumbling in"],
+"homeVerb":["Hailing", "Coming", "Originating"],
+"homeSentence":["#lowVerb# from #lowHome#", "#highVerb# from #highHome#", "#homeVerb# from #home#"],
+"featureVerb":["brandishes", "flashes", "displays", "flaunts", "waves", "boasts", "readies", "exposes", "readies", "reveals"],
+"featureAdjective":["fearsome", "impressive", "shiny", "sharp", "wide", "acidic", "toxic", "flaming", "blazing", "burning", "frozen", "frosted", "spiky", "large", "massive", "intimidating", "spindly", "avian", "feline", "canine", "imposing", "snapping"],
+"featureNoun":["claws", "fangs", "teeth", "wings", "tentacles", "limbs", "arms", "fists", "legs", "hands", "feet", "paws", "tail", "form", "stature", "body"],
+"feature":["#featureAdjective# #featureNoun#", "#featureAdjective#, #featureAdjective# #featureNoun#"],
+"featureSentence":["#featureVerb# its #feature#"],
+"monsterAdj":["feared", "respected", "revered", "known", "infamous", "venerated", "notorious", "detested", "hated", "loathed", "despised", "aborred"],
+"attackAdj":["sweeping", "paralyzing", "blazing", "flaming", "explosive", "burning", "electric", "heavy", "crushing", "freezing", "treacherous", "devastating", "piercing", "toxic", "acidic"],
+"attackNoun":["blast", "beam", "slash", "slice", "whip strike", "flail", "slam", "horde of minions", "ground pound", "bomb", "gas cloud", "projectile"],
+"attack":["#attackAdj.a# #attackNoun#", "#attackAdj.a#, #attackAdj# #attackNoun#"],
+"monster":["#homeSentence#, #monsterName# approaches. The pilots scan the monster and watch as it #featureSentence#; #featureSentence#; #featureSentence#; and #featureSentence#. #monsterName# is #monsterAdj# for its signature attack: #attack#"],
+"origin":["#[monsterName:#name#]monster#"]
+}
