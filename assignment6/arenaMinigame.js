6f1b33fe70135fb1d5deaf5ec29259d46908d4c5
nimtiaz-ucsc <nimtiaz@ucsc.edu>
2023-05-23 21:45:51 -0700

    added enemy dino levels

--- assignment6/arenaMinigame.js
+++ assignment6/arenaMinigame.js
@@ -6,15 +6,28 @@ function runArenaMinigame() {
     var currentDino = document.getElementById("dinoSelect").value;
 
     game.context.drawImage(document.getElementById(currentDino + "Sprite"), game.dinoX - (dinoSize/2), game.dinoY - (dinoSize/2), dinoSize, dinoSize);
+    game.context.fillStyle = "black";
     game.context.font = "16px Times New Roman";
     game.context.fillText("Lv. " + eval("game."+currentDino), game.dinoX - (dinoSize/2), game.dinoY - (dinoSize/2));
 
-    if (Math.random() < .15){
-        game.enemies.push({x:Math.floor(Math.random()*game.canvas.width), y:Math.floor(Math.random()*game.canvas.height), dirX:0, dirY:0, sprite: game.dinosaurs[Math.floor(Math.random()*game.dinosaurs.length)]})
-        //console.log(game.enemies)
+    if (Math.random() < .15 && game.enemies.length <= 3){
+        var max = game.canvas.height - dinoSize;
+
+        game.enemies.push({x:game.canvas.width, y:Math.floor(Math.random()* (max - dinoSize) + dinoSize), sprite: game.dinosaurs[Math.floor(Math.random()*game.dinosaurs.length)], level: Math.floor(Math.random() * ((game.highestLevel * 1.5) - 1) + 1)})
       }  
     game.enemies.forEach(function (e){
-        game.context.drawImage(document.getElementById(e.sprite + "Sprite"), e.x - 16, e.y - 16, 32, 32);
+        e.x -= 5;
+        game.context.drawImage(document.getElementById(e.sprite + "Sprite"), e.x - (dinoSize/2), e.y - (dinoSize/2), dinoSize, dinoSize);
+        if (e.level > eval("game." + currentDino)) {
+            game.context.fillStyle = "red";
+        } else {
+            game.context.fillStyle = "green";
+        }
+        game.context.fillText("Lv. " + e.level, e.x - (dinoSize/2), e.y - (dinoSize/2));
+
+        if (e.x <= 0) {
+            game.enemies.splice(game.enemies.indexOf(e), 1);
+        }
     })
 }
 
